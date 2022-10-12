7fc7a334bd0e114de0adeaab071e530fb72bdded
vazha <example@gamil.com>
2022-10-12 19:04:03 +0400

    test6

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <active-user
+    :name="contacts.name"
+    :age="contacts.age"
+    >
+    </active-user>
+    <user-data
+    @add-person="addNewContact">
+    </user-data>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+           contacts:{
+                    id: "",
+                    name: "User Name: ",
+                    age: "User Age: "
+                },
+            }
+    },
+    methods: {
+        addNewContact(name, age){
+            this.contacts = {
+                id: new Date().toISOString(),
+                name: name,
+                age: age
+            };
+        }
+    }
+
+}
+</script>
+
+<style>
+
+    html {
+        font-family: sans-serif;
+    }
+
+    section {
+        margin: 2rem auto;
+        max-width: 40rem;
+        border-radius: 12px;
+        border: 1px solid #ccc;
+        padding: 1rem;
+    }
+</style>
---
+++ src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <label>Name</label>
+        <input type="text" v-model="enteredName">
+        <label> Age</label>
+        <input type="text"  v-model="enteredAge">
+        <button @click="addPerson">Add</button>
+    </section>
+</template>
+
+<script>
+export default {
+    emits: ['add-person'],
+    data(){
+        return{
+            enteredName: "",
+            enteredAge: ""
+        }
+    },
+    methods: {
+        addPerson(){
+            this.$emit("add-person", this.enteredName, this.enteredAge);
+        }
+    }
+}
+</script>
